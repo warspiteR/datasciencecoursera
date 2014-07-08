9932f2db1de7afacdb4bd71447b1d3e130623605
warspiteR <dumont@xs4all.nl>
2014-07-08 16:28:05 +0200

    supporting documentation, and added setwd

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,4 @@
-
-#1. Merges the training and the test sets to create one data set.
+setwd("~/Documents/Coursera/datasciencecoursera") #please change when needed.
 
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 #working on a mac, so need to define the method
